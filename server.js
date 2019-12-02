a248699a4d8b13d235f55678820653589dd23ee1
ibraim-Ajarmeh <ajarmeh1997@gmail.com>
2019-12-02 14:28:02 +0200

    starting to get the data from the APIs

--- server.js
+++ server.js
@@ -7,47 +7,48 @@ const superagent = require('superagent');
 const pg = require('pg');
 const unirest = require("unirest");
 
-
 const client = new pg.Client(process.env.DATABASE_URL)
 const PORT = process.env.PORT || 8080;
 const app = express();
+
 
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
 
-
 app.get('/', proofOfLife);
 app.get('/dictionary', getFromDictionary)
 app.get('/newTerm', NewWordForm)
-app.post('/addNew', addNewWord)
+app.post('/addNewCard', renderForm)
+app.post('/dictionary', addNewWord)
 app.get('/test', data)
 
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
-function data(params) {
+function data(req, res) {
 
-    var req = unirest("GET", "https://wordsapiv1.p.rapidapi.com/words/hatchback/typeOf");
-
-    req.headers({
-        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
-        "x-rapidapi-key": "861a23e564msh9e59c3b41ee0870p1bc725jsn80dfae1d8670"
-    });
+    console.log('testing');
+    const app_id = "d6324635"; // insert your APP Id
+    const app_key = "285981e5b60037743d3e6301a5a386a3"; // insert your APP Key
 
 
-    req.end(function (res) {
-        // if (res.error) throw new Error(res.error);
-
-        console.log(res.body);
-    });
+    let url = `https://od-api.oxforddictionaries.com/api/v2/entries/${lang}/${input}?fields=pronunciations&strictMatch=false`;
+    superagent.get(url)
+        .set('app_id', app_id)
+        .set('app_key', app_key)
+        .set('Accept', 'application/json')
+        .then(data => {
+            res.json(data.body);
+        })
+        .catch(console.error);
 }
 
 
 function proofOfLife(req, res) {
-    // res.status(200).send(`hello form our app`)
-    res.render('pages/info')
+    res.status(200).send(`hello form our app`)
+    // res.render('pages/info')
 }
 
 function getFromDictionary(req, res) {
@@ -58,6 +59,13 @@ function getFromDictionary(req, res) {
             console.log('res', results.rows[0]);
 
         })
+}
+
+function renderForm(req, res) {
+    let { term, description, image_url, notes } = req.body
+    // console.log('gorob', req.body);
+
+    res.render('pages/add', { card: req.body })
 }
 
 function NewWordForm(req, res) {
@@ -71,7 +79,7 @@ function NewWordForm(req, res) {
 }
 
 function addNewWord(req, res) {
-    let SQL = `INSERT INTO words (term, discription, image_url, notes) VALUES ($1,$2,$3,$4) RETURNING *`;
+    let SQL = `INSERT INTO words (term, description, image_url, notes) VALUES ($1,$2,$3,$4) RETURNING *`;
     let values = ['dog', 'mammal animal', 'https://i.dailymail.co.uk/1s/2019/11/23/09/21370544-7717313-image-a-1_1574501083030.jpg', 'hello doggoy'];
     client.query(SQL, values)
         .then(results => {
